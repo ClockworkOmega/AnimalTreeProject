0dff946b104b0b57ab54dbe2808eac3f4baa3266
ClockworkOmega <crf2431@hotmail.com>
2017-04-24 16:00:24 -0400

    Add files via upload

---
+++ Version1_AnimalTree/AnimalTree.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef string Elem;
+
+struct Node
+{
+	Elem value;
+	Node* parent;
+	Node* left;
+	Node* right;
+	vector<Node>* child;
+};
+
+class AnimalTree
+{
+public:
+	AnimalTree() { root_ == NULL; }
+	~AnimalTree() {};
+	Elem& operator*() const;
+	Node parent() const;
+	vector<Node> children() const;
+	int size() const;
+	bool empty() const;
+	bool isRoot() const;
+	bool isExternal() const;
+	Node root() const; //Get the root
+	// We need a positions function
+	// Which returns the positions of all nodes
+	// positions () const; <- example
+
+private:
+	Node* root_;
+	int size_;
+
+};
