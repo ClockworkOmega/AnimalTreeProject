757f7cce2db7be3181d92c3780c1c023de83a3eb
ClockworkOmega <crf2431@hotmail.com>
2017-05-03 17:42:06 -0400

    Update AnimalTree.h

--- Code Not Yet Approved/Chris/AnimalTree.h
+++ Code Not Yet Approved/Chris/AnimalTree.h
@@ -10,38 +10,33 @@ typedef string Elem;
 
 struct Node
 {
-	//int nodeID;
 	Elem value;
 	Node* parent;
 	vector<Node*> child;
 };
 
-class AnimalTree
+class Tree
 {
 public:
-	AnimalTree() { root_ = NULL; }
-	~AnimalTree() {};
-	void currentNode() const;				// Returns current node ID & value
-	void parent() const;					// Return parent ID & value of current
-	void children(const Elem& searchVal);			// Return string value of each node in current
+	Tree() { root_ = NULL; }
+	~Tree() {};
+	void currentNode() const;					// Returns current value
+	void parent(const Elem& searchVal);				// Return parent ID & value of current
+	void children(const Elem& searchVal);				// Return string value of each node in current
 	int size() const;						// Return size of tree
 	bool empty() const;						// Return true if empty
 	Node root() const;						// Get the root string value
-	bool isLeaf() const;					// Return true if current node is a leaf
-	bool isRoot() const;					// Return true if current node is root
+	Node* rootVal();						// Return the root node
+	bool isLeaf(const Elem& searchVal);				// Return true if current node is a leaf
+	bool isRoot(const Elem& searchVal);				// Return true if current node is root
 
-	Node* searchNodeString(const Elem& val, Node* subtree);				// Search for a particular node by it's string value
-	void addRoot(const Elem& val);										// Add a root node to the tree
-	void insert(const Elem& searchVal, const Elem& val);				// Create a child for the current node
-	void search(const Elem& val);										// Find a node and set it to current
-	void readIn(Node& root);
+	Node* searchNodeString(const Elem& val, Node* subtree);		// Search for a particular node by it's string value
+	void addRoot(const Elem& val);					// Add a root node to the tree
+	void insert(const Elem& searchVal, const Elem& val);		// Create a child for the current node
+	void search(const Elem& val);					// Find a node and set it to current
 
-	Node* rootVal();
-	Node* currentVal();
+	void printTree(Tree test);
 
-	// TEST FUNCTIONS
-	void testSearch(const Elem& val);
-	void childSize(const Elem& val);
 
 private:
 	Node* root_;
